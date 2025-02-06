fed7beab8e02fa8ffd8cbe75d2d2db63ba8da0ab
agusNazer <agustin.nazer@hotmail.com>
2025-02-06 20:10:38 -0300

    desafio2 finished

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         
+        // creo instancias de personas
         Persona persona1 = new Persona("Juan", "Perez");
         Persona persona2 = new Persona("Pablo", "Ramirez");
         Persona persona3 = new Persona("Lucia", "Gonzalez");
@@ -17,6 +18,7 @@ public class App {
         // System.out.println("Persona 4: " + persona4.getNombre() + " " + persona4.getApellido() );
         // System.out.println("Persona 5: " + persona5.getNombre() + " " + persona5.getApellido() );
         
+        // Creo la lista, y agrego las personas a ella
         List<Persona> listaPersonas = new ArrayList<>();
         listaPersonas.add(persona1);
         listaPersonas.add(persona2);
@@ -25,17 +27,35 @@ public class App {
         listaPersonas.add(persona5);
 
         //mosrtar con un for in la lista.
-        for(Persona individuo : listaPersonas){
-        System.out.println(individuo.getNombre() + " " + individuo.getApellido());
-        }
+        // for(Persona individuo : listaPersonas){
+        // System.out.println(individuo.getNombre());
+        // }
         
-        // Ordenar por nombre (A-Z)
+        // ordenado por nombre(A-Z)
         listaPersonas.sort(Comparator.comparing(Persona::getNombre));
 
         System.out.println("\nOrdenado por Nombre (A-Z):");
         for (Persona individuo : listaPersonas) {
-        System.out.println(individuo.getNombre() + " " + individuo.getApellido());
+        System.out.println(individuo.getNombre());
         }
+
+        // ordenado por apellido(A-Z)
+        listaPersonas.sort(Comparator.comparing(Persona::getApellido));
+
+        System.out.println("\nOrdenado por Apellido (A-Z):");
+        for (Persona individuo : listaPersonas) {
+        System.out.println(individuo.getApellido());
+        }
+
+        // ordenado por apellido inverso(Z-A)
+        listaPersonas.sort(Comparator.comparing(Persona::getApellido).reversed());
+
+        System.out.println("\nOrdenado por Apellido inverso (Z-A):");
+        for (Persona individuo : listaPersonas) {
+        System.out.println(individuo.getApellido());
+        }
+
+        
 
     }
 
